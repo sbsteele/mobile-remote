22b41250d8e97aabd0ea503d49db7f2f5dae2f46
Scott Steele <scott@sbsteele.com>
2014-07-05 18:07:28 -0500

    Added light control page and page swipe control

--- MyApp/public/javascripts/dvd.js
+++ MyApp/public/javascripts/dvd.js
@@ -1,6 +1,35 @@
+$(function () {
+  $("[data-role=panel]").enhanceWithin().panel();
+});
+
 $(function(){
   $( ".cmd" ).click(function() {
     alert( "Handler called cmd = " + $( this ).attr( "id" ));
     $.post( "/", { cmd: $ (this).attr( "id" ) });
   });
+});
+
+$(document).on('swipeleft', '.ui-page', function(event){    
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {    
+        var nextpage = $.mobile.activePage.next('[data-role="page"]');
+        // swipe using id of next page if exists
+        if (nextpage.length > 0) {
+            $.mobile.changePage(nextpage, {transition: "slide", reverse: false}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;         
+});
+
+$(document).on('swiperight', '.ui-page', function(event){     
+    if(event.handled !== true) // This will prevent event triggering more then once
+    {      
+        var prevpage = $(this).prev('[data-role="page"]');
+        if (prevpage.length > 0) {
+            $.mobile.changePage(prevpage, {transition: "slide", reverse: true}, true, true);
+        }
+        event.handled = true;
+    }
+    return false;            
 });
